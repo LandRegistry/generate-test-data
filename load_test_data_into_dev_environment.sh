d313db471f315d1ad27a8a1f308404ba608754ef
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-08-04 10:53:04 +0100

    Fixed for new dev environment

--- load_test_data_into_dev_environment.sh
+++ load_test_data_into_dev_environment.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [[ -f ./dev/run-command ]]; then
-	echo "It looks like you're using the old dev environment"
-	./dev/run-command python loader.py $@
-else
-	echo "Running with new dev environment"
-	source /vagrant/script/dev-env-functions
-	create_virtual_env "generate-test-data"
-	python loader.py $@
-	deactivate
-fi
+echo "Running with new dev environment"
+source /vagrant/script/dev-env-functions
+create_virtual_env "generate-test-data"
+python loader.py $@
+deactivate
